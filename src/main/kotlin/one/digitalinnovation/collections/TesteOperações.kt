d01950cbbf2417948423eca56afa9defd489e795
Luis <luisfrr88@gmail.com>
2022-01-05 12:58:40 -0300

    Aplicando Conceitos de coleções,Arrays,Listas

--- src/main/kotlin/one/digitalinnovation/collections/TesteOperações.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperações.kt
@@ -1,7 +1,7 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val salarios = doubleArrayOf(1000.0, 2500.0, 5800.0)
+    val salarios = doubleArrayOf(1000.0, 2250.0, 3500.0, 4000.0)
 
     for (salario in salarios){
     println(salario)
@@ -15,4 +15,13 @@ fun main() {
     println("-----------------------")
     salariosMaiorque2500.forEach{println(it)}
 
+    println("-----------------------")
+    println(salarios.count{it in 2500.0..5000.0})
+    println("-----------------------")
+    println(salarios.find{it == 2250.0})
+    println(salarios.find{it == 500.0})
+
+    println("-----------------------")
+    println(salarios.any {it == 1000.0})
+    println(salarios.any {it == 500.0})
 }
