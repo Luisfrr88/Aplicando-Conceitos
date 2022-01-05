04a634a76fa5d3f2f2aaacddd2090d36ca53c94f
Luis <luisfrr88@gmail.com>
2022-01-05 11:14:35 -0300

    create array

---
+++ src/main/kotlin/one/digitalinnovation/collections/TestintArray.kt
@@ -0,0 +1,29 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val values = IntArray(5)
+    values[0] = 1
+    values[1] = 4
+    values[2] = 7
+    values[3] = 2
+    values[4] = 5
+
+    for (valor in values) {
+        println(valor)
+    }
+    println("-----------------------------------------------")
+    values.forEach {
+    println(it)
+    }
+    println("-----------------------------------------------")
+    for (index in values.indices) {
+        println(values[index])
+
+    }
+    println("-----------------------------------------------")
+    values.sort()
+    for (valor in values) {
+        println(valor)
+    }
+
+}
